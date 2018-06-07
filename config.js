f846028f528d6f59c643f124b832bf1122c5511d
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-06-07 16:56:45 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = "https://api.npool.pw/mkt";
+
+var poolHost = "ca.npool.pw";
+
+var blockExplorer = "http://explorer.marketcash.io";
+
+var cryptonatorWidget = ["BTC-USD", "MKT-BTC"];
+
+var blockchainExplorer = "http://explorer.marketcash.io/block.html?hash={id}";
+
+var transactionExplorer = "http://explorer.marketcash.io/transaction.html?hash={id}";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = [
+        ["npool.pw/mkt", "https://api.npool.pw/mkt"]
+];
